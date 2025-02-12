5115625a4985ebc877c399e8cc5d2c1501146596
John Shutt <pemulis@users.noreply.github.com>
2025-02-12 14:45:00 -0800

    add Event to the polyfill, in addition to CustomEvent
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- polyfill.cjs
+++ polyfill.cjs
@@ -1,4 +1,13 @@
+// polyfill.cjs
 const { JSDOM } = require('jsdom');
-const { window } = new JSDOM();
-globalThis.CustomEvent = window.CustomEvent;
-console.log("CustomEvent polyfill via jsdom applied (CommonJS).");
+
+// Create a new JSDOM instance. Using pretendToBeVisual can sometimes help produce a more complete DOM.
+const dom = new JSDOM('', { pretendToBeVisual: true });
+
+// Set global Event and CustomEvent from the same window
+globalThis.Event = dom.window.Event;
+globalThis.CustomEvent = dom.window.CustomEvent;
+
+console.log("Polyfilled Event and CustomEvent via jsdom.");
+// For debugging: verify that CustomEvent is an instance of Event
+console.log("Test: new CustomEvent('test') instanceof Event =", new CustomEvent('test') instanceof Event);
