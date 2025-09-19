4981c2b4c47dc47510aeb3a65c287371cfc07e2f
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2025-09-19 16:04:43 -0500

    ➕ Add TSDoc Plugin to ESLint
    Added and configured TSDoc plugin for ESLint for formatting TSDoc comments

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import tsdoc from 'eslint-plugin-tsdoc'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		extends: ['js/recommended'],
+		languageOptions: { globals: globals.browser },
+		plugins: { js, tsdoc },
+		rules: {
+			'tsdoc/syntax': 'error',
+		},
+	},
+	tseslint.configs.recommended,
+])
